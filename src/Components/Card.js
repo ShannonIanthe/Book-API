36ef4e83a40337f5e0a101028ca96108508764a2
Shannon Mettry <shannonmettry12@gmail.com>
2022-11-16 14:52:31 +0100

    adding http request with axios

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,16 +1,24 @@
 import react from 'react';
 import './style.css'
 
-const Card=() => {
+const Card=({book}) => {
+    console.log(book)
     return (
         <>
-<div className="card">
+        {
+            book.map((item) => {
+                return(
+                    <div className="card">
 <img src="./images/book.jpeg" alt="" />
 <div className="bottom">
     <h3 className='title'>React Js</h3>
     <p className='amount'>&#x20AC;24</p>
 </div>
 </div>
+                )
+            })
+        }
+
 
         </>
     )
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,9 +1,21 @@
-import react from 'react'
+import react, {useState} from 'react'
 import Card from './Card'
 import './style.css'
 import { BiSearchAlt } from 'react-icons/bi'
+import axios from 'axios'
 
 const Main =()=> {
+    const [search, setSearch] = useState("")
+    const [bookData, setData] = useState([])
+    const searchBook=(evt) => {
+        if(evt.key==="Enter") 
+        {
+            axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyC3_Dzf4oh_G3M1AAKrE6wBKx-zuJ0qk4w')
+            .then(res=>setData(res.data.items))
+            .catch(err=>console.log(err))
+        }
+    }
+
     return (
         <>
             <div className='header'>
@@ -13,14 +25,19 @@ const Main =()=> {
                 <div className='row2'>
                     <h2>Find Your Book</h2>
                     <div className="search">
-                        <input type="text" placeholder='Enter Book Name' />
+                        <input type="text" placeholder='Enter Book Name'
+                        value={search} onChange={e=>setSearch(e.target.value)} 
+                            onKeyPress={searchBook}
+                        />
                         <button><BiSearchAlt/></button>
                     </div>
                     <img src='./images/book1.jpg' alt='picture-of-books'/>
                 </div>
             </div>
             <div className="container">
-                <Card/>
+                {
+                    <Card book={bookData}/>
+                }
             </div>
         </>
     )
